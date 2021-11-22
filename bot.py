47aed420ad4ae3a2203d4935c7679b0ad3307e5d
Pdiskbot <93695686+Pdiskbot@users.noreply.github.com>
2021-11-22 23:10:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ Here is your  [Short Link]({short_link})''', quote=True)
 
 
 async def get_shortlink(link):
-    url = 'https://www.vivurl.com/api'
+    url = 'https://www.pdiskshortner.net/api'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
